c8c40dd65af0c366a25ef8ebdfba9f59f5b029c3
Christoph Pfister <cpfister@yahoo.com>
2025-07-04 10:03:36 -0700

    changed default sensor polling interval

--- src/settings.ts
+++ src/settings.ts
@@ -12,17 +12,17 @@ export const PLUGIN_NAME = 'homebridge-purpleair-sensor';
 /**
  * This is the default update interval in seconds - 5 minutes.
  */
-export const DEFAULT_UPDATE_INTERVAL_SECS = 5 * 60;
+export const DEFAULT_UPDATE_INTERVAL_SECS = 60 * 60;
 
 /**
  * Never update more frequently than the following value - 30 seconds.
  */
-export const MIN_UPDATE_INTERVAL_SECS = 30;
+export const MIN_UPDATE_INTERVAL_SECS = 300;
 
 /**
  * Timeout after which to consider a sensor or API failed - 1 hour.
  */
-export const SENSOR_FAILURE_TIMEOUT_SECS = 60 * 60;
+export const SENSOR_FAILURE_TIMEOUT_SECS = 300 * 60;
 
 /**
  * The base URL for the PurpleAir API.
